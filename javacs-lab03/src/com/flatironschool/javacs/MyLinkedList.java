2b38eade4915c1e480eba3038abe0b3fe1d16d94
Samuel Lair <lair001@gmail.com>
2017-04-03 23:04:12 -0400

    Done.

--- javacs-lab03/src/com/flatironschool/javacs/MyLinkedList.java
+++ javacs-lab03/src/com/flatironschool/javacs/MyLinkedList.java
@@ -69,7 +69,6 @@ public class MyLinkedList<E> implements List<E> {
 		System.out.println(Arrays.toString(mll.toArray()) + " size = " + mll.size());
 	}
 
-	@Override
 	public boolean add(E element) {
 		if (head == null) {
 			head = new Node(element);
@@ -83,12 +82,26 @@ public class MyLinkedList<E> implements List<E> {
 		return true;
 	}
 
-	@Override
 	public void add(int index, E element) {
 		// TODO: fill this in
+		if (index < 0 || index > this.size()) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (index == this.size()) {
+			this.add(element);
+		} else if (index == 0) {
+			this.head = new Node(element, this.head);
+			this.size++;
+		} else {
+			Node prevNode = head;
+			for (int i=0; i < index-1; i++) {
+				prevNode = prevNode.next;
+			}
+			prevNode.next = new Node(element, prevNode.next);
+			this.size++;
+		}
 	}
 
-	@Override
 	public boolean addAll(Collection<? extends E> collection) {
 		boolean flag = true;
 		for (E element: collection) {
@@ -97,23 +110,19 @@ public class MyLinkedList<E> implements List<E> {
 		return flag;
 	}
 
-	@Override
 	public boolean addAll(int index, Collection<? extends E> collection) {
 		throw new UnsupportedOperationException();
 	}
 
-	@Override
 	public void clear() {
 		head = null;
 		size = 0;
 	}
 
-	@Override
 	public boolean contains(Object obj) {
 		return indexOf(obj) != -1;
 	}
 
-	@Override
 	public boolean containsAll(Collection<?> collection) {
 		for (Object obj: collection) {
 			if (!contains(obj)) {
@@ -123,7 +132,6 @@ public class MyLinkedList<E> implements List<E> {
 		return true;
 	}
 
-	@Override
 	public E get(int index) {
 		Node node = getNode(index);
 		return node.cargo;
@@ -144,9 +152,15 @@ public class MyLinkedList<E> implements List<E> {
 		return node;
 	}
 
-	@Override
 	public int indexOf(Object target) {
 		// TODO: fill this in
+		int index = 0;
+		for (Node node = head; node != null; node = node.next) {
+			if (equals(target, node.cargo)) {
+				return index;
+			}
+			index++;
+		}
 		return -1;
 	}
 
@@ -165,18 +179,15 @@ public class MyLinkedList<E> implements List<E> {
 		}
 	}
 
-	@Override
 	public boolean isEmpty() {
 		return size == 0;
 	}
 
-	@Override
 	public Iterator<E> iterator() {
 		E[] array = (E[]) toArray();
 		return Arrays.asList(array).iterator();
 	}
 
-	@Override
 	public int lastIndexOf(Object target) {
 		Node node = head;
 		int index = -1;
@@ -189,29 +200,52 @@ public class MyLinkedList<E> implements List<E> {
 		return index;
 	}
 
-	@Override
 	public ListIterator<E> listIterator() {
 		return null;
 	}
 
-	@Override
 	public ListIterator<E> listIterator(int index) {
 		return null;
 	}
 
-	@Override
 	public boolean remove(Object obj) {
 		// TODO: fill this in
+		Node prevNode = null;
+		for (Node node = this.head; node != null; node = node.next) {
+			if (this.equals(obj, node.cargo)) {
+				if (prevNode != null) {
+					prevNode.next = node.next;
+				} else {
+					this.head = node.next;
+				}
+				this.size--;
+				return true;
+			}
+			prevNode = node;
+		}
 		return false;
 	}
 
-	@Override
 	public E remove(int index) {
 		// TODO: fill this in
-		return null;
+		if (index < 0 || index >= this.size()) {
+			throw new IndexOutOfBoundsException();
+		}
+		Node prevNode = null;
+		Node node = head;
+		for (int i=0; i<index; i++) {
+			prevNode = node;
+			node = node.next;
+		}
+		if (prevNode != null) {
+			prevNode.next = node.next;
+		} else {
+			this.head = node.next;
+		}
+		size--;
+		return node.cargo;
 	}
 
-	@Override
 	public boolean removeAll(Collection<?> collection) {
 		boolean flag = true;
 		for (Object obj: collection) {
@@ -220,12 +254,10 @@ public class MyLinkedList<E> implements List<E> {
 		return flag;
 	}
 
-	@Override
 	public boolean retainAll(Collection<?> collection) {
 		throw new UnsupportedOperationException();
 	}
 
-	@Override
 	public E set(int index, E element) {
 		Node node = getNode(index);
 		E old = node.cargo;
@@ -233,12 +265,10 @@ public class MyLinkedList<E> implements List<E> {
 		return old;
 	}
 
-	@Override
 	public int size() {
 		return size;
 	}
 
-	@Override
 	public List<E> subList(int fromIndex, int toIndex) {
 		if (fromIndex < 0 || toIndex >= size || fromIndex > toIndex) {
 			throw new IndexOutOfBoundsException();
@@ -255,7 +285,6 @@ public class MyLinkedList<E> implements List<E> {
 		return list;
 	}
 
-	@Override
 	public Object[] toArray() {
 		Object[] array = new Object[size];
 		int i = 0;
@@ -267,7 +296,6 @@ public class MyLinkedList<E> implements List<E> {
 		return array;
 	}
 
-	@Override
 	public <T> T[] toArray(T[] a) {
 		throw new UnsupportedOperationException();		
 	}
